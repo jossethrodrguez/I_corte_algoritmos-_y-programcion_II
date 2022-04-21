545fe7513d2901cd9d94eaaed94ef5db138a6f60
Josseth Rodriguez <hfernand0382@gmail.com>
2022-04-21 09:38:51 -0400

    starting the converter asking what do you want to convert

---
+++ dollars-bolivars-converter/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int dolares, bolivares, eleccion;
+
+    printf("escribe '0' para convertir de dolares a bolivares \nescribe '1' para convertir de bolivares a dolares ");
+    scanf("%d", &eleccion);
+
+    printf("%d", eleccion);
+}
