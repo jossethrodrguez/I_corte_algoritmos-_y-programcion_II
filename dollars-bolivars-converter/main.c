28c23061345e0e2cd4dd5ed917bd9d69e82d2f11
Josseth Rodriguez <hfernand0382@gmail.com>
2023-07-21 22:57:58 -0400

    adding the covertion to Dolars

--- dollars-bolivars-converter/main.c
+++ dollars-bolivars-converter/main.c
@@ -5,15 +5,27 @@ int main()
 {
     float tasaDeCambio = 5.85, operacion;
     int eleccion, cantidad;
-
+\
+    printf("Conversor de Dolar a Bolivares (tasa de cambio a 5.85Bs por Dolar)\n");
     printf("escribe '0' para convertir de dolares a bolivares \nescribe '1' para convertir de bolivares a dolares\n:  ");
     scanf("%d", &eleccion);
 
-    if (eleccion = 1){
+
+    if(eleccion == 0){
         printf("Cuantos Dolares quieres convertir a Bolivars ? : ");
         scanf("%d", &cantidad);
 
         operacion = cantidad * tasaDeCambio;
         printf("%d son %f\n", cantidad, operacion);
+    }
+    else if(eleccion == 1){
+        printf("Cuantos Bolivares quieres convertir a Dolares ? : ");
+        scanf("%d", &cantidad);
+
+        operacion = cantidad / tasaDeCambio;
+        printf("%d son %f\n", cantidad, operacion);
+    }
+    else{
+        printf("Por favor Ingresa un caracter Valido");
     }
 }
