1627b8f7e2b2655a71d410fab81b5d44cb7b00e9
Josseth Rodriguez <hfernand0382@gmail.com>
2023-07-21 21:17:14 -0400

    adding the dolar to bolivars converter

--- dollars-bolivars-converter/main.c
+++ dollars-bolivars-converter/main.c
@@ -3,10 +3,17 @@
 
 int main()
 {
-    int dolares, bolivares, eleccion;
+    float tasaDeCambio = 5.85, operacion;
+    int eleccion, cantidad;
 
-    printf("escribe '0' para convertir de dolares a bolivares \nescribe '1' para convertir de bolivares a dolares ");
+    printf("escribe '0' para convertir de dolares a bolivares \nescribe '1' para convertir de bolivares a dolares\n:  ");
     scanf("%d", &eleccion);
 
-    printf("%d", eleccion);
+    if (eleccion = 1){
+        printf("Cuantos Dolares quieres convertir a Bolivars ? : ");
+        scanf("%d", &cantidad);
+
+        operacion = cantidad * tasaDeCambio;
+        printf("%d son %f\n", cantidad, operacion);
+    }
 }
